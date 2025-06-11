0308ab234e3d514cdf34287f1b6675ebdae56ee8
Joe Anthony <jwanthon@mtu.edu>
2025-06-11 15:52:19 -0400

    Updated IntegratinTestbench.m

--- Testbenches/IntegrationTestbench.m
+++ Testbenches/IntegrationTestbench.m
@@ -4,7 +4,7 @@ addpath('Functions\');
 %  Joseph Anthony
 %
 % Created:         5/29/25
-% Last Modified:   6/02/25
+% Last Modified:   6/11/25
 %
 % Description: Testbench for different integration techniques for 
 %   calculating FEM stiffness matrices with large quantities of basis 
@@ -16,8 +16,9 @@ addpath('Functions\');
 % Required:
 %   Symbolic Math Toolbox           
 
-n = 20; % Mesh size
+n = 30; % Mesh size
 L = 10;  % Length  (L) [m]
+modeCount = 5;
 
 %% Generating Basis Functions
 % Create vector of test function polynomials and their derivative
@@ -64,9 +65,9 @@ deltax = L/(n+1);
 xvals = linspace(deltax, n*deltax, n);
 
 % Determine the effect of each derivative function on the string
-beamBasis = zeros(n,n);
+DbeamBasis = zeros(n,n);
 for i = 1:n
-    beamBasis(i,:) = ht(deltax*i);
+    DbeamBasis(i,:) = ht(deltax*i);
 end
 toc
 %% Generate Stiffness Matrix - Poly Method
@@ -107,10 +108,66 @@ K = zeros(n);
 for col = 1:n
     for row = 1:col
         % Determine the current product function
-        temp = beamBasis(row,:).*beamBasis(col,:);
+        temp = DbeamBasis(row,:).*DbeamBasis(col,:);
         % Find the trap rule sum of this beam
         K(row, col) = trapz(xvals, temp);
     end
 end
+K = K + K' - diag(diag(K));
 
 toc
+%% Graph FEM Matrix
+% Find and sort eigenvectors
+[eigVecs, eigVals] = eig(K);
+[d, index] = sort(diag(eigVals));
+eigVals = eigVals(index,index);
+eigVecs = eigVecs(:, index);
+eigVals = flip(flip(eigVals, 1), 2);
+eigVecs = flip(eigVecs, 1);
+
+% Turn basis functions into usable form for MATLAB
+ht = zeros(1,n);
+for i = 1:n
+    ht = matlabFunction(phi(i));
+end
+deltax = L/(n+1);
+
+% Determine the effect of each basis function on the string
+beamBasis = zeros(n,n);
+for i = 1:n
+    beamBasis(i,:) = ht(deltax*i);
+end
+disp('Basis function string effects calculated.');
+
+% Normalize the basis functions
+for i = 1:n
+    beamBasis(:,i) = beamBasis(:,i)/max(abs(beamBasis(:,i)));
+end
+
+% Create the mode shapes by scaling the basis functions by each eigenvector
+modeShapes = beamBasis*eigVecs;
+disp('Mode shapes calculated.');
+
+% Plot modecount eigenvectors
+hold on;
+for i = 1:modeCount
+     plot(linspace(0,L,n+2),[0, modeShapes(:,i)', 0],"Marker",".")
+end
+title(sprintf('First %d Mode Shapes of a BI-BI Beam', modeCount));
+xlabel('Beam Position [m]');
+ylabel('Displacement [m]');
+hold off
+%% Integration vs Traps Values
+syms testfn(x)
+testfn = 1;
+for i = 1:3
+    testfn = plus(testfn, rand*x^i);
+end
+val1 = int(testfn, [0,10])
+ht = matlabFunction(testfn);
+fnout = zeros(1,10);
+for i = 1:100
+    fnout(i) = ht(i/10);
+end
+val2 = trapz(fnout, [0.1:0.1:10])
+diff = abs(val1-val2)
